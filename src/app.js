e3a201a24c6a422ed561a9ef7dc30a7a2c0d155c
rodoabad <rodoabad@gmail.com>
2015-04-25 17:25:02 -0500

    Updated 'app.js' so Angular is manually started.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 (function() {
   'use strict';
 
-  var app = angular.module('app', ['ui.router']);
+  angular.module('app', ['ui.router']);
 
-  app.config(function($stateProvider, $urlRouterProvider) {
+  angular.config(function($stateProvider, $urlRouterProvider) {
 
     // Default route
     $urlRouterProvider.otherwise('/');
@@ -14,5 +14,7 @@
         templateUrl: 'src/home/controllers/home.html'
       });
   });
+
+  angular.bootstrap(document, ['app]);
 
 })();
