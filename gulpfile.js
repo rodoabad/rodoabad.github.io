007858b79c138a9df11df8561515ae54077a6468
rodoabad <rodoabad@gmail.com>
2015-04-25 19:08:47 -0500

    Added watchers to all JS directories and the 'gulpfile.js' itself.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,39 @@
-'use strict';
+(function() {
+  'use strict';
 
-// Reguire the following
+  // Reguire the following
 
-var gulp = require('gulp'),
-    jshint = require('gulp-jshint');
+  var gulp = require('gulp'),
+      jshint = require('gulp-jshint'),
+      connect = require('gulp-connect');
 
-// Lint *.js files
-gulp.task('lint', function() {
-  return gulp.src('src/**/*.js')
-      .pipe(jshint())
-      .pipe(jshint.reporter('jshint-stylish'))
-      .pipe(jshint.reporter('fail'));
-});
+  var dirs = {
+    js: [
+      'gulpfile.js',
+      'src/**/*.js'
+    ]
+  };
 
-// Watch files for changes
-gulp.task('watch', function() {
-  gulp.watch('src/**/*.js', ['lint']);
-});
+  // Lint *.js files
+  gulp.task('lint', function() {
+    return gulp.src(dirs.js)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'));
+  });
 
-gulp.task('default', ['lint', 'watch']);
+  // Start preview server
+  gulp.task('preview-server', function() {
+    connect.server({
+      livereload: true
+    });
+  });
+
+  // Watch files for changes
+  gulp.task('watch', function() {
+    gulp.watch(dirs.js, ['lint']);
+  });
+
+  gulp.task('default', ['lint', 'watch']);
+
+})();
