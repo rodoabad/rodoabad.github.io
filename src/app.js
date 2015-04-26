4f6233f8ac5a171fa2309eff19e17966d579ab0f
rodoabad <rodoabad@gmail.com>
2015-04-25 20:37:51 -0500

    Added 'ocLazyLoad'.

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular
-    .module('app', ['ui.router'])
+    .module('app', ['ui.router', 'oc.lazyLoad'])
     .config(function($stateProvider, $urlRouterProvider) {
 
     // Default route
@@ -11,7 +11,20 @@
     $stateProvider
       .state('home',{
         url: '/',
-        templateUrl: 'src/home/home.html'
+        templateUrl: 'src/home/home.html',
+        // Resolve the following before instantiating a view.
+        // https://github.com/angular-ui/ui-router/wiki
+        resolve: {
+          // 'controllers' name is meaningless. It could be anything you want.
+          controllers: ['$ocLazyLoad', function($lazy) {
+            return $lazy.load({
+              name: 'HomeController',
+              files: [
+                'src/home/HomeController.js'
+              ]
+            });
+          }]
+        }
       });
   });
 
