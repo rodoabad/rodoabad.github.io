050d2ef5ff256f555908a76700363817d77df87c
rodoabad <rodoabad@gmail.com>
2015-06-20 18:45:23 -0500

    Added Karma config file.

---
+++ tests/karma.conf.js
@@ -0,0 +1,35 @@
+module.exports = function ( config ) {
+
+  'use strict';
+
+  config.set ( {
+
+    basePath: '../',
+
+    frameworks: [
+      'chai',
+      'mocha'
+    ],
+
+    files: [
+
+      // Library Test
+      'lib/angular/angular.js',
+      'lib/angular/angular-mocks.js',
+      'lib/angular-ui-router/angular-ui-router.js',
+      'lib/ocLazyLoad/ocLazyLoad.js',
+
+      'src/**/*{.spec.js,.js,}'
+    ],
+
+    reporters: [ 'mocha' ],
+
+    port: 9876,
+    colors: true,
+
+    logLevel: config.LOG_INFO,
+
+    browsers: [ 'PhantomJS' ]
+
+  } )
+};
